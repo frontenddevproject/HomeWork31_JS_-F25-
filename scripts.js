9b49c1216aba15fcd92f1f45abb9338a421648b0
frontenddevproject <frontenddevproject@gmail.com>
2023-05-05 21:32:09 +0300

    added changes to setInterval()

--- scripts.js
+++ scripts.js
@@ -4,15 +4,17 @@ const resetBtn = document.querySelector("#reset");
 const rocketImage = document.querySelector(".image");
 const visualBlock = document.querySelector(".visual-content");
 
+let timer = 0;
+
 class RocketLauncher {
-   constructor(checkSystems, fuelQuantity) {
+   constructor(fuelQuantity, checkSystems) {
       this.fuelQuantity = fuelQuantity;
       this.checkSystems = checkSystems;
    }
  
    rocketPreparation () {
       this.fuelQuantity = 0;
-      this.rocketRefueling(this.fuelQuantity);
+      this.rocketRefueling();
 
       const fuel = document.querySelector(".fuel");
       const fuelCounter = document.querySelector(".fuel-counter");
@@ -20,40 +22,41 @@ class RocketLauncher {
 
       textBlock.innerHTML = "";
 
-         setInterval( () => {
-            console.log(`${this.fuelQuantity} %`);
-            this.fuelQuantity += 10; 
-            
-            fuelCounter.textContent = `${this.fuelQuantity} %`;
-            fuel.style.width = `${this.fuelQuantity}%`;
+      timer = setInterval( () => {
+               console.log(`${this.fuelQuantity} %`);
+               this.fuelQuantity += 10; 
+               
+               fuelCounter.textContent = `${this.fuelQuantity} %`;
+               fuel.style.width = `${this.fuelQuantity}%`;
 
-            if (this.fuelQuantity === 50) {
-               fuelCounter.style.color = "#fff";
-            }    
+               if (this.fuelQuantity === 50) {
+                  fuelCounter.style.color = "#fff";
+               }    
 
-            if (this.fuelQuantity === 100) {
-               clearInterval (this.fuelQuantity === 100);
-               textBlock.innerHTML = "<h2>Checking of the rocket systems</h2>"
-               setTimeout(() => {
-                  this.checkOfFuelSystem(textBlock).checkOfControlSystem(textBlock).checkOfCoolingSystem(textBlock);
-                  console.log(this);
-              
-               if (this.checkSystems.every((el) => el === true)) {
-                  textBlock.innerHTML += "<h2>Press START to launch the rocket</h2>"
-                  startBtn.removeAttribute("disabled");
-                  startBtn.style.background = "green"
-               } else {
-                  console.log ("Start is not possible, not all systems are ready");
-                  }
-               }, 2000);
+               if (this.fuelQuantity === 100) {
+                  clearInterval (timer);
 
-               return this;
-            }
-         }, 500);
-    
+                  textBlock.innerHTML = "<h2>Checking of the rocket systems</h2>"
+   
+                  setTimeout(() => {
+                     this.checkOfFuelSystem(textBlock).checkOfControlSystem(textBlock).checkOfCoolingSystem(textBlock);
+                     console.log(this);
+                  
+                     if (this.checkSystems.every((el) => el === true)) {
+                        textBlock.innerHTML += "<h2>Press START to launch the rocket</h2>"
+                        startBtn.removeAttribute("disabled");
+                        startBtn.style.background = "green"
+                     } else {
+                        console.log ("Start is not possible, not all systems are ready");
+                        }
+                  }, 2000);
+
+                  return this;
+               }
+            }, 500);
    }
 
-   rocketRefueling (fuelQuantity) {
+   rocketRefueling () {
       visualBlock.innerHTML = "";
       visualBlock.innerHTML = `
       <h2> The fuel level in the rocket tanks</h2> 
